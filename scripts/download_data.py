69e01c226a2dd541b5ac497592f4b384d7417160
hugojarkoff <hugo.jarkoff@gmail.com>
2024-05-05 12:23:23 +0200

    Data downloading script

---
+++ scripts/download_data.py
@@ -0,0 +1,15 @@
+# Make sure your kaggle.json API token is correctly located in ~/.kaggle dir
+
+from pathlib import Path
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+DATA_PATH = Path("data")
+DATA_PATH.mkdir(exist_ok=True, parents=True)
+
+api = KaggleApi()
+api.dataset_download_files(
+    dataset="adibhabbou/french-rap-lyrics",
+    path=DATA_PATH,
+    unzip=True,
+    force=True,
+)
