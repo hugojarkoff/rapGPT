4e6d1fbd69f4919968dab82f94134a41766796b7
hugojarkoff <hugo.jarkoff@gmail.com>
2024-05-08 19:30:20 +0200

    Refactor data module

---
+++ src/rapgpt/data.py
@@ -0,0 +1,31 @@
+from rapgpt.config import Settings
+from pathlib import Path
+
+
+class Corpus:
+    def __init__(self, settings: Settings) -> None:
+        self.data_path: Path = Path(settings.data.path)
+        # TODO: Move this assertion to Settings init?
+        assert self.data_path.exists()
+        self.artists: list[Path] = list(self.data_path.glob("*.txt"))
+
+
+class ArtistLyrics:
+    @classmethod
+    def from_file(cls, filename: Path) -> str:
+        return filename.read_text()
+
+
+class Artist:
+    def __init__(self, artist_file: Path):
+        self.name: str = artist_file.name.strip(".txt").capitalize()
+        self.lyrics = ArtistLyrics.from_file(artist_file)
+
+
+if __name__ == "__main__":
+    # TODO: Put in unit test
+    settings = Settings()
+    corpus = Corpus(settings)
+    artist = Artist(corpus.artists[0])
+    print("artist name: ", artist.name)
+    print("artist lyrics: ", artist.lyrics[:100])
