6311b473e0340c6c76fe2d9b9d4a32cc7275efb7
hugojarkoff <hugo.jarkoff@gmail.com>
2024-05-08 18:20:26 +0200

    Refactor download data script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -6,10 +6,16 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 DATA_PATH = Path("data")
 DATA_PATH.mkdir(exist_ok=True, parents=True)
 
-api = KaggleApi()
-api.dataset_download_files(
-    dataset="adibhabbou/french-rap-lyrics",
-    path=DATA_PATH,
-    unzip=True,
-    force=True,
-)
+
+def main():
+    api = KaggleApi()
+    api.dataset_download_files(
+        dataset="adibhabbou/french-rap-lyrics",
+        path=DATA_PATH,
+        unzip=True,
+        force=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
