cfd16fe93e9aede7386026b94a1d2e69e5183936
hugojarkoff <hugo.jarkoff@gmail.com>
2024-05-16 14:51:39 +0200

    Put config file as CLI arg for training

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,21 @@
 from rapgpt.trainer import Trainer
 from rapgpt.config import Config
+import argparse
 
-if __name__ == "__main__":
-    config = Config()
+
+def main():
+    parser = argparse.ArgumentParser(description="Training args.")
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="Path to toml config file",
+        default="configs/config.toml",
+    )
+    args = parser.parse_args()
+    config = Config.load_from_toml(args.config)
     trainer = Trainer(config)
     trainer.train()
+
+
+if __name__ == "__main__":
+    main()
