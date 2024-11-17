b6968faf28535a0f59a530c48b47180e3798f39c
hugojarkoff <hugo.jarkoff@gmail.com>
2024-11-17 16:31:16 +0100

    Update push_to_hub script

--- scripts/push_to_hf_hub.py
+++ scripts/push_to_hf_hub.py
@@ -3,6 +3,7 @@ from rapgpt.config import Config
 from rapgpt.model import HFHubTransformerModel
 from rapgpt.encoder import Encoder
 import argparse
+from huggingface_hub import HfApi
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -27,12 +28,29 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    api = HfApi()
+
+    api.upload_file(
+        path_or_fileobj=args.artists_tokens,
+        path_in_repo="artists_tokens.txt",
+        repo_id="hugojarkoff/rapgpt",
+        repo_type="model",
+    )
+
     with open(args.artists_tokens, "r") as f:
         artists_tokens = {
             line.split(":")[0]: int(line.split(":")[1].rstrip("\n")) for line in f
         }
 
+    api.upload_file(
+        path_or_fileobj=args.config_file,
+        path_in_repo="config.toml",
+        repo_id="hugojarkoff/rapgpt",
+        repo_type="model",
+    )
+
     config = Config.load_from_toml(args.config_file)
+
     encoder = Encoder(config=config)
 
     model = HFHubTransformerModel(
